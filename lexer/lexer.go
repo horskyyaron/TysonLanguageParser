2a2061b0cde71032d3fdbe2640317da3dc3e10cc
Yaron Horsky <horsky.yaron@gmail.com>
2025-03-24 19:37:43 +0200

    added more operators

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,6 +31,18 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 
 	switch l.ch {
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
@@ -97,4 +109,3 @@ func (l *Lexer) readNumber() string {
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
-
--- token/token.go
+++ token/token.go
@@ -14,8 +14,17 @@ const (
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1343456
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
