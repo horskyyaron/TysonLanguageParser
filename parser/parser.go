76df2e1e8e197f81f12bf9fb05caaaa138707df0
Yaron Horsky <horsky.yaron@gmail.com>
2025-04-07 19:47:35 +0300

    added tests and tracing functionality

--- parser/parser.go
+++ parser/parser.go
@@ -9,8 +9,7 @@ import (
 )
 
 const (
-	int = iota
-	_
+	_int = iota
 	LOWEST
 	EQUALS      // ==
 	LESSGREATER // > or <
@@ -59,6 +58,7 @@ func New(l *lexer.Lexer) *Parser {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
+	defer untrace(trace("parsePrefixExpression"))
 	expression := &ast.PrefixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
@@ -68,7 +68,26 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 	return expression
 }
 
+// NOTE: this will turn + to be right associatvie
+// func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+// 	expression := &ast.InfixExpression{
+// 		Token:    p.curToken,
+// 		Operator: p.curToken.Literal,
+// 		Left:     left,
+// 	}
+// 	precedence := p.curPrecedence()
+// 	p.nextToken()
+// 	if expression.Operator == "+" {
+// 		expression.Right = p.parseExpression(precedence - 1)
+// 	} else {
+// 		expression.Right = p.parseExpression(precedence)
+// 	}
+// 	return expression
+// }
+
+// NOTE: this keeps + left associatvie
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	defer untrace(trace("parseInfixExpression"))
 	expression := &ast.InfixExpression{
 		Token:    p.curToken,
 		Operator: p.curToken.Literal,
@@ -123,6 +142,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	defer untrace(trace("parseExpressionStatement"))
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 	stmt.Expression = p.parseExpression(LOWEST)
 	if p.peekTokenIs(token.SEMICOLON) {
@@ -132,6 +152,7 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 }
 
 func (p *Parser) parseExpression(precedence int64) ast.Expression {
+	defer untrace(trace("parseExpression"))
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -210,6 +231,7 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
+	defer untrace(trace("parseIntegerLiteral"))
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
 	if err != nil {
